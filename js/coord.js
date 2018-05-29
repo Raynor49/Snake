2cec6439b22b9268e690c945d64a09acb23be6be
Willis Roberts <willisroberts49@gmail.com>
2018-05-29 15:04:46 -0400

    coord class for the snake

---
+++ js/coord.js
@@ -0,0 +1,15 @@
+export default class Coord{
+  constructor(x=0, y=0){
+    this.x = x
+    this.y = y
+  }
+
+  plus(coord2){
+    this.x += coord2.x
+    this.y += coord2.y
+  }
+
+
+
+
+}
--- js/snake.js
+++ js/snake.js
@@ -1,10 +1,27 @@
+import Coord from './coord.js';
+
 export default class Snake{
   constructor() {
     this.direction = "N"
-    this.headPos = [0,0]
+    this.segmentPositions = [new Coord(0,0)]
   }
 
-  
+  head(){
+    return this.segmentPositions.last
+  }
+
+  move() {
+    this.segmentPositions.push(this.head().plus(DIRECTIONS[this.direction]))
+  }
 
 
+
+
+}
+
+DIRECTIONS = {
+  "N" => new Coord(0,1),
+  "S" => new Coord(0,-1),
+  "E" => new Coord(1,0),
+  "W" => new Coord(-1,0)
 }
