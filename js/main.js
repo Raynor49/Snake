d679b3bf6ef7eeae0d01a41c140edd9f9f4a404d
Willis Roberts <willisroberts49@gmail.com>
2018-06-04 15:07:29 -0400

    Snake draw method, create game class

--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
-// import _ from 'lodash';
-// function component() {
-//   var element = document.createElement('div');
-//
-//   // Lodash, currently included via a script, is required for this line to work
-//   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//
-//   return element;
-// }
-//
-// document.body.appendChild(component());
-$(function () => {
+import Snake from './snake.js'
+
+$(function () {
   let canvas = $(".canvas")
   let ctx = canvas.getContext("2d")
+
 })
--- js/snake.js
+++ js/snake.js
@@ -1,17 +1,33 @@
 import Coord from './coord.js';
-
 export default class Snake{
   constructor() {
     this.direction = "N"
     this.segmentPositions = [new Coord(0,0)]
   }
 
+  draw(ctx){
+    for (var i = 0; i < this.segmentPositions.length; i++) {
+      ctx.beginPath();
+      ctx.arc(
+        this.segmentPositions[i][0], this.segmentPositions[i][1], 3, 0, 2*Math.PI, false
+      );
+      ctx.fillStyle = 'black'
+      ctx.fill()
+      ctx.closePath();
+    }
+  }
+
   head(){
     return this.segmentPositions.last
   }
 
+  grow(){
+    this.segmentPositions.unshift()
+  }
+
   move() {
     this.segmentPositions.push(this.head().plus(DIRECTIONS[this.direction]))
+    this.segmentPositions.shift()
   }
 
 
@@ -19,9 +35,9 @@ export default class Snake{
 
 }
 
-DIRECTIONS = {
-  "N" => new Coord(0,1),
-  "S" => new Coord(0,-1),
-  "E" => new Coord(1,0),
-  "W" => new Coord(-1,0)
+Snake.DIRECTIONS = {
+  "N" : new Coord(0,1),
+  "S" : new Coord(0,-1),
+  "E" : new Coord(1,0),
+  "W" : new Coord(-1,0)
 }
